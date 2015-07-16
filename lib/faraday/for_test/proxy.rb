05e75a86fd29df1e2dd3dc148c4f9bed5b1fb94d
Kunio Okita <okitakunio@gmail.com>
2015-07-16 20:52:15 +0900

    make it clear

--- lib/faraday/for_test/proxy.rb
+++ lib/faraday/for_test/proxy.rb
@@ -10,7 +10,7 @@ module Faraday
         if @client.respond_to?(name)
           maybe_response = @client.__send__(name, *args, &block)
 
-          if maybe_response.is_a?(Faraday::Response)
+          if maybe_response.is_a?(::Faraday::Response)
             response = Faraday::ForTest::Response.new(maybe_response)
             response.request_params = args.find {|e| e.is_a?(Array) || e.is_a?(Hash) || e.is_a?(NilClass) }
             response
