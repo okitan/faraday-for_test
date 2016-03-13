670fd7c3222f875b693828c26f862d40411502df
okitan <okitakunio@gmail.com>
2016-03-13 17:15:17 +0900

    configuration is done in each class

--- lib/faraday/for_test/response/formatter.rb
+++ lib/faraday/for_test/response/formatter.rb
@@ -1,50 +1,66 @@
 require "faraday/for_test/response"
 
-class Faraday::ForTest::Response
-  module Formatter
-    # TODO: actual http version
-    def request_line
-      [ env[:method].upcase, env[:url], "HTTP/1.1" ].join(" ") + "\n"
+require "rack/utils"
+
+module Faraday::ForTest
+  class Configuration
+    def body_formatters
+      @body_formatters ||= {
+        "application/x-www-form-urlencoded" => Rack::Utils.method(:build_query)
+      }
     end
 
-    def request_headers
-      env[:request_headers].map {|k, v| "#{k}: #{v}" }.join("\n")
-    end
-
-    # TODO: configuraable
-    def request_body(pretty = false)
-      if request_params && !request_params.empty?
-        if formatter = Faraday::ForTest.configuration.body_formatters.find {|k, v| env[:request_headers]["Content-Type"] =~ /#{k}/ }
-          formatter.last.call(request_params)
-        else
-          request_params.inspect
-        end
-      else
-        ""
-      end + "\n"
-    end
-
-    # TODO: reason-phase
-    def status_line
-      [ "HTTP/1.1", status ].join(" ") + "\n"
-    end
-
-    def response_headers
-      headers.map {|k, v| "#{k}: #{v}" }.join("\n")
-    end
-
-    def response_body(pretty = false)
-      if body && !body.empty?
-        if formatter = Faraday::ForTest.configuration.body_formatters.find {|k, v| headers["content-type"] =~ /#{k}/ }
-          formatter.last.call(body)
-        else
-          body.inspect
-        end
-      else
-        ""
-      end + "\n"
+    def add_body_formatter(key, proc = nil, &block)
+      body_formatters[key] = block_given? ? block : proc
     end
   end
-  
-  include Formatter
+
+  class Response
+    module Formatter
+      # TODO: actual http version
+      def request_line
+        [ env[:method].upcase, env[:url], "HTTP/1.1" ].join(" ") + "\n"
+      end
+
+      def request_headers
+        env[:request_headers].map {|k, v| "#{k}: #{v}" }.join("\n")
+      end
+
+      # TODO: configuraable
+      def request_body(pretty = false)
+        if request_params && !request_params.empty?
+          if formatter = Faraday::ForTest.configuration.body_formatters.find {|k, v| env[:request_headers]["Content-Type"] =~ /#{k}/ }
+            formatter.last.call(request_params)
+          else
+            request_params.inspect
+          end
+        else
+          ""
+        end + "\n"
+      end
+
+      # TODO: reason-phase
+      def status_line
+        [ "HTTP/1.1", status ].join(" ") + "\n"
+      end
+
+      def response_headers
+        headers.map {|k, v| "#{k}: #{v}" }.join("\n")
+      end
+
+      def response_body(pretty = false)
+        if body && !body.empty?
+          if formatter = Faraday::ForTest.configuration.body_formatters.find {|k, v| headers["content-type"] =~ /#{k}/ }
+            formatter.last.call(body)
+          else
+            body.inspect
+          end
+        else
+          ""
+        end + "\n"
+      end
+    end
+
+    include Formatter
+  end
 end
